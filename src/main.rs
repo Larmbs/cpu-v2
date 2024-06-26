731da40a47eb6adffbe7f008d15d1d788bdee0db
Larmbs <liamisrael200@gmail.com>
2024-06-26 15:13:01 -0400

    Made a CLI for program

--- src/main.rs
+++ src/main.rs
@@ -1,189 +1,36 @@
-//! The DroneBoi computer project
-//! By Larmbs
+mod comp;
+use std::env;
+use std::fs;
+use std::path;
 
-/// RAM Storage
-/// -----------
-/// 16 addresses with 16 bits each equalling 16X16 bits
-/// When creating RAM DroneBoi does not allow for setting
-/// individual function values in editor
-/// Therefore this cannot store instructions
-struct RAM {
-    mem: [u16; 16],
-}
-impl RAM {
-    fn new() -> Self {
-        RAM { mem: [0u16; 16] }
-    }
-    fn read(&self, addr: usize) -> u16 {
-        assert!(addr < self.mem.len(), "Address provided was out of range");
-        self.mem[addr]
-    }
-    fn write(&mut self, addr: usize, val: u16) {
-        assert!(addr < self.mem.len(), "Address provided was out of range");
-        self.mem[addr] = val;
-    }
-}
+const DBEXEC_EXT: &str = "dbexec";
 
-/// Program ROM Storage
-/// -------------------
-/// This storage is solely meant for program memory and will store
-/// the entirety of the program
-/// This follows the Harvard Architecture https://en.wikipedia.org/wiki/Harvard_architecture
-/// As we`ve seen before ROM can be extremely 1 block for 3X16 bits, therefore we will
-/// give a big ROM size that is some multiple of 3
-/// For a prog length of 64 lines ish we will need 21 function blocks
-struct ProgROM {
-    mem: [u16; 64],
-}
-impl ProgROM {
-    fn from(mem: [u16; 64]) -> Self {
-        ProgROM { mem }
-    }
-    fn read(&self, addr: usize) -> u16 {
-        assert!(addr < self.mem.len(), "Address provided was out of range");
-        self.mem[addr]
-    }
-}
+const FILE_CONTAINS_ERROR: &str = "DBEXEC File Contains Some Error";
+const INVALID_PATH: &str = "Invalid Path";
+const NOT_DBEXEC_FILE: &str = "File Must End In .dbexec";
+const READ_ERROR: &str = "Issue Reading File";
 
-/// ALU Module
-/// ----------
-/// This module will be kept as simple as possible to minimize the projects complexity
-/// there will be no integer overflow protection or anything else.
-/// All this will have is binary operators and simple binary addition and subtraction.
-/// This means it will have only 8 operations.
-struct ALU;
-impl ALU {
-    /// WARNING
-    /// This will return unexpected values when overflow occurs
-    fn exec(mode: u8, a: u16, b: u16) -> u16 {
-        assert!(
-            mode < 11,
-            "Mode provided was invalid, modes go from 0 to {} exclusive",
-            11
-        );
-
-        match mode {
-            0 => a << b, // Left Shift
-            1 => a >> b, // Right Shift
-            2 => a & b,  // AND
-            3 => !a,     // NOT
-            4 => a ^ b,  // XOR
-            5 => a | b,  // OR
-            6 => a + b,  // Addition
-            7 => a - b,  // Subtraction
-            8 => ((a > b) as u16  ) << 3 | ((a < b) as u16) << 2 | ((a == b) as u16) << 1 | 1,
-            9 => a + 1,
-            10 => a - 1,
-            _ => will_never_happen(),
-        }
-    }
-}
-
-/// We will run the CPU here, CPU is just a control unit and is sort of messy
 fn main() {
-    // Initializing modules
-    let mut ram = RAM::new();
-    let rom = ProgROM::from([016; 64]); // Replace this with program values
+    let mut args = env::args();
+    args.next();
 
-    // Registers
-    /* Private */
-    let mut prog_cnt: u16 = 0; // Program counter, tells computer what instruction to read
-    /* Public */
-    let alu_a: u16 = 0; // The A value to be passed into the ALU
-    let alu_b: u16 = 0; // The B value to be passed into the ALU
-    let alu_o: u16 = 0; // The output register of ALU
-    let flags: u16 = 0; // Register with a multitude of flags
-    let base: u16 = 0;  // Base addr when using RAM
-    let gpr1: u16 = 0;  // General purpose register no unique use
-    let gpr2: u16 = 0;  // General purpose register no unique use
-
-    // Putting all regs in one array to make things simpler to visualize
-    let mut regs = [0, alu_a, alu_b, alu_o, flags, base, gpr1, gpr2];
-
-    // Looping over and over
-    loop {
-        // Getting instruction
-        let instr = rom.read(prog_cnt as usize);
-
-        // 16 different possible operations, bit mask and shift to isolate 4 first bits.
-        let opcode: u8 = ((instr & 0b1111_0000_0000_0000) >> 12) as u8;
-
-        match opcode {
-            0 => {
-                // ALU operation
-                let mode = instr & 0b1111_1111_1111;
-                regs[3] = ALU::exec(mode as u8, regs[1], regs[2]);
-            },
-            1 => {
-                // LOAD operation
-
-                // Bit-masking out args
-                let addr = (instr & 0b1111_1111) as usize; // First 8 bits is addr
-                let reg = ((instr & 0b1111_0000_0000) >> 8) as usize; // Middle 4 bits is reg addr
-
-                regs[reg] = ram.read(addr);
-            },
-            2 => {
-                // STORE operation
-
-                // Bit-masking out args
-                let addr = (instr & 0b1111_1111) as usize; // First 8 bits is addr
-                let reg = ((instr & 0b1111_0000_0000) >> 8) as usize; // Middle 4 bits is reg addr
-
-                ram.write(addr, regs[reg]);
-            },
-            3 => {
-                // MOVE instruction
-
-                // Bit-masking out args
-                let reg_copy = ((instr & 0b1111_0000) >> 8) as usize;
-                let reg_recv = ((instr & 0b1111) >> 4) as usize;
-
-                regs[reg_recv] = regs[reg_copy];
-            },
-            4 => {
-                // JUMP instruction
-            
-                // Bit-masking jump value
-                let jump_size = instr & 0b0001_1111_1111;
-                let jump_sign = (instr & 0b0010_0000_0000) >> 9;
-                let jump_type = (instr & 0b1100_0000_0000) >> 10;
-                
-                let res = regs[4] & (1 << jump_type);
-
-                // Jumps if flag is true
-                if res == 1 {
-                    // Jumping in different direction depending on jump sign
-                    match jump_sign {
-                        0 => prog_cnt += jump_size,
-                        1 => prog_cnt -= jump_size,
-                        _ => will_never_happen()
-                    }
-                }
-            },
-            5 => {
-                // LOAD IMD instruction
-                // 8 bit constant value
-                let value = instr & 0b1111_1111;
-                let reg = (instr & 0b1111_0000_0000) as usize;
-
-                regs[reg] = value;
-                
-            },
-            6 => {
-                // HALT Instruction
-                break;
-            },
-            _ => will_never_happen(),
-        }
-        
-        // Incrementing program counter
-        prog_cnt += 1;
+    // Parsing path out
+    let file_path: path::PathBuf = args.next()
+        .expect(INVALID_PATH)
+        .parse()
+        .expect(INVALID_PATH);
+    if file_path.extension().unwrap() != DBEXEC_EXT {
+        eprintln!("{}", NOT_DBEXEC_FILE);
     }
-}
 
-#[inline]
-#[allow(unused)]
-fn will_never_happen() -> ! {
-    panic!("Congratulation in getting this function to run!")
+    // Getting the actual code in correct form
+    let string = fs::read_to_string(file_path).expect(READ_ERROR);
+    let code: Vec<u16> = string.lines().map(|line| line.trim().parse().expect(FILE_CONTAINS_ERROR)).collect();
+    let prog: [u16; 64] = code.try_into().expect(FILE_CONTAINS_ERROR);
+
+    // Simulating
+    let output = comp::run(prog);
+
+    // Outputting debug info
+    println!("{}", output);
 }
